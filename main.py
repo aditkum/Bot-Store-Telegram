8d6f987a149b2ef8dcaaf623740e25400f6d4709
cleoinhere <aadityakumarsahroy@gmail.com>
2025-07-13 19:03:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from bot.handlers.user import user
 from bot.modules.payment_handler import payment_gateway
 from bot.modules.utils import utils
 from pymongo import MongoClient, ASCENDING
+from bot.handlers.user import start, handle_vip
+
+   application.add_handler(CommandHandler("start", start))
+   application.add_handler(CommandHandler("vip", handle_vip))
+   
 from telegram import (
     Update,
     InlineKeyboardButton,
