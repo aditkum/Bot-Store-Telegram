659944a03e6543fd6ae22ee73df561ee8e85d723
cleoinhere <aadityakumarsahroy@gmail.com>
2025-07-13 18:37:08 +0700

    Update utils.py

--- bot/modules/utils.py
+++ bot/modules/utils.py
@@ -1,16 +1,16 @@
-   # File utils.py yang benar
+# /root/Bot-Store-Telegram/bot/modules/utils.py
+
 def format_currency(amount):
-       """Format angka ke format mata uang Indonesia"""
+    """Format angka ke format mata uang Indonesia."""
     return f"Rp{amount:,.0f}".replace(",", ".")
 
 def validate_phone(number):
-       """Validasi nomor telepon Indonesia"""
+    """Validasi nomor telepon Indonesia."""
     import re
     return re.match(r"^\+62\d{9,12}$", number) is not None
 
-   # Ekspor fungsi-fungsi
-   utils = {
-       'format_currency': format_currency,
-       'validate_phone': validate_phone
-   }
-   
+# Ekspor fungsi-fungsi
+utils = {
+    'format_currency': format_currency,
+    'validate_phone': validate_phone
+}
