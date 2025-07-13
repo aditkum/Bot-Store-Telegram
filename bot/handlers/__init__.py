455ec35439cd9d5493279a276d696ded63db95d3
cleoinhere <aadityakumarsahroy@gmail.com>
2025-07-13 17:24:05 +0700

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,1 +1,23 @@
+# Export handler dari modul ke package level
+from .admin import (
+    menu as admin_menu,
+    handle_admin_callback,
+    show_stats,
+    show_recent_transactions
+)
+from .user import (
+    start,
+    handle_vip,
+    handle_profile
+)
+
+__all__ = [
+    'admin_menu',
+    'handle_admin_callback',
+    'show_stats',
+    'show_recent_transactions',
+    'start',
+    'handle_vip',
+    'handle_profile'
+]
 
